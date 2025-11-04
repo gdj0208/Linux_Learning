38637c6b5b9f40d1ebdb2b0ca6ba9da60f7b1b26
gdj0208 <gdj0208@gmail.com>
2025-11-04 22:40:56 +0900

    merged from chpt9

--- 과제/Testfolder/file1.c
+++ 과제/Testfolder/file1.c
@@ -1,16 +1,29 @@
+#include <ftw.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <dirent.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/utsname.h>
 #include <sys/stat.h>
+#include <sys/unistd.h>
+#include <sys/time.h>
+#include <sys/times.h>
+#include <pwd.h>
+#include <time.h>
 
-int main() {
-    DIR * dp;
-    struct dirent *dent;
 
-    dp = opendir(".");
-    while(dent = readdir(dp)) {
-        struct stat st;
-        stat(dent->d_name, &st);
+int main(int argc, char *argv[]) {
+    
+    int fd, n;
+    char buf[BUFSIZ];
 
-        
+    fd = open("test.txt", O_RDONLY);
+    
+    while((n = read(fd, buf, 1)) > 0) {
+        write(1, buf, 1);
+        lseek(fd, 1, SEEK_CUR);
     }
 }
